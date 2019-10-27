c618e44671d7c96eaa138a8853280f5616b74fcf
yevhenii.ovcharenko <yevhenii.ovcharenko@masterofcode.com>
2019-10-27 20:22:01 +0200

    full coverage for the handlersChampLeague

--- FootballManagerAPI/src/__tests__/API/v1/champLeague/handlersChampLeague.spec.js
+++ FootballManagerAPI/src/__tests__/API/v1/champLeague/handlersChampLeague.spec.js
@@ -26,19 +26,19 @@ champLeague.selectFromChampLeague = jest.fn(() => promiseResolved);
 champLeague.updateChampLeague = jest.fn(() => Promise.resolve({ status: 'UPDATED' }));
 champLeague.deleteChampLeague = jest.fn(() => Promise.resolve({ status: 'DELETED' }));
 
-describe('handlers champions league tests - positive', () => {
-  const data = {
-    groupName: 'hello',
-    games: 5,
-    wins: 5,
-    draws: 5,
-    loses: 5,
-    goals: 5,
-    missed: 5,
-    scoredBalls: 5,
-    teamId: 5,
-  };
+const data = {
+  groupName: 'hello',
+  games: 5,
+  wins: 5,
+  draws: 5,
+  loses: 5,
+  goals: 5,
+  missed: 5,
+  scoredBalls: 5,
+  teamId: 5,
+};
 
+describe('handlers champions league tests - positive', () => {
   it('should return OK from champLeague table after inserting data', async () => {
     const result = await handlersChampLeague.addChampLeague(data);
     expect(result).toEqual({ message: 'Created new Champ League', status: 200 });
@@ -84,5 +84,36 @@ describe('handlers champions league tests - positive', () => {
   });
 });
 
-// describe('handlers champions league tests - neggative', () => {
-// });
+describe('handlers champions league tests - neggative', () => {
+  it('should handle an error while execution insert queries ', async () => {
+    champLeague.insertChampLeague = jest
+      .fn()
+      .mockImplementationOnce(() => Promise.reject(new Error('Failed to execute query')));
+    const result = await handlersChampLeague.addChampLeague();
+    expect(result).toEqual({ message: 'Bad request', status: 400 });
+  });
+
+  it('should handle an error while execution select queries ', async () => {
+    champLeague.selectFromChampLeague = jest
+      .fn()
+      .mockImplementationOnce(() => Promise.reject(new Error('Failed to execute query')));
+    const result = await handlersChampLeague.geChampLeague();
+    expect(result).toEqual({ message: 'Bad request', status: 400 });
+  });
+
+  it('should handle an error while execution update queries ', async () => {
+    champLeague.updateChampLeague = jest
+      .fn()
+      .mockImplementationOnce(() => Promise.reject(new Error('Failed to execute query')));
+    const result = await handlersChampLeague.updateChampLeague();
+    expect(result).toEqual({ message: 'Bad request', status: 400 });
+  });
+
+  it('should handle an error while execution delete queries ', async () => {
+    champLeague.deleteChampLeague = jest
+      .fn()
+      .mockImplementationOnce(() => Promise.reject(new Error('Failed to execute query')));
+    const result = await handlersChampLeague.deleteChampLeague();
+    expect(result).toEqual({ message: 'Bad request', status: 400 });
+  });
+});
